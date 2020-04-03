90154d2fbb19d5809daf453fb53a7d354ede4ceb
krustowski <krusty@n0p.cz>
2020-04-03 12:26:29 +0200

    renaming the whole repo -> prod.sh minor changes

--- prod.sh
+++ prod.sh
@@ -4,8 +4,8 @@
 # skript pro efektivni aktualizaci textovky na produkci
 # by krusty / 28. 3. 2020
 
-cd && tar -czf textovka.tar.gz textovka/ && \
+cd && tar -czf textovka.tar.gz textovka-api/ && \
  scp ~/textovka.tar.gz frank:~/ && \
- ssh frank 'rm -rf ~/textovka && tar -xzf ~/textovka.tar.gz && chmod o+w ~/textovka/data ~/textovka/game.log' && \
+ ssh frank 'rm -rf ~/textovka-api && tar -xzf ~/textovka.tar.gz && chmod o+w ~/textovka-api/data ~/textovka-api/game.log' && \
  echo "ok" || \
  echo "fail"
