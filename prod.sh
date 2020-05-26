ffc42c407f94af2736250754216a4822d4816aa4
krustowski <krusty@n0p.cz>
2020-05-26 15:21:44 +0200

    prod.sh / some read permissions deleted

--- prod.sh
+++ prod.sh
@@ -6,6 +6,6 @@
 
 cd && tar -czf textovka.tar.gz textovka-api/ && \
  scp ~/textovka.tar.gz frank:~/ && \
- ssh frank 'rm -rf ~/textovka-api && tar -xzf ~/textovka.tar.gz && chmod o+w ~/textovka-api/data ~/textovka-api/game.log' && \
+ ssh frank 'rm -rf ~/textovka-api && tar -xzf ~/textovka.tar.gz && chmod o+w ~/textovka-api/data ~/textovka-api/game.log && chmod o-rx ~/textovka-api/game.log ~/textovka-api/README.md ~/textovka-api/prod.sh' && \
  echo "ok" || \
  echo "fail"
