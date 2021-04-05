6887611d4dde0394082c5b84db56614c712754d8
krustowski <krusty@n0p.cz>
2021-04-05 22:52:33 +0200

    +composer repo

--- public/index.php
+++ public/index.php
@@ -10,9 +10,7 @@ namespace textovka;
 
 defined("ROOT_DIR") || define("ROOT_DIR", __DIR__ . "/..");
 
-// load all classes
-foreach (glob(ROOT_DIR . "/src/*.php") as $filename) {
-    include_once $filename;
-}
+// composer load
+require ROOT_DIR . "/vendor/autoload.php";
 
 new Game();
